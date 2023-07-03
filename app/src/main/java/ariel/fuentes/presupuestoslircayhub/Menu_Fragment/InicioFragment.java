c9e64de0cb500f0c107aa6b902a3dd1873f3879b
Falcon_109 <benja.fuente123@gmail.com>
2023-07-02 21:01:18 -0400

    Punto de guardado - El InicioFragment ya esta empezando a funcionar solo falta comprarlo con el total de los gastos

--- app/src/main/java/ariel/fuentes/presupuestoslircayhub/Menu_Fragment/InicioFragment.java
+++ app/src/main/java/ariel/fuentes/presupuestoslircayhub/Menu_Fragment/InicioFragment.java
@@ -1,66 +1,72 @@
 package ariel.fuentes.presupuestoslircayhub.Menu_Fragment;
-
+import android.content.SharedPreferences;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+import androidx.preference.PreferenceManager;
 
 import ariel.fuentes.presupuestoslircayhub.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link InicioFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class InicioFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private TextView textViewPresupuestoTotal;
+    private TextView textViewResumenArriendo;
+    private TextView textViewResumenAlimentacion;
+    private TextView textViewResumenTransporte;
+    private TextView textViewResumenSerBasicos;
+    private TextView textViewResumenEducacion;
+    private TextView textViewResumenDeudas;
+    private TextView textViewResumenAhorros;
+    private TextView textViewResumenOtros;
 
     public InicioFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SecondFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static InicioFragment newInstance(String param1, String param2) {
-        InicioFragment fragment = new InicioFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_inicio, container, false);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inicio, container, false);
+        textViewPresupuestoTotal = view.findViewById(R.id.PresupuestoTotal);
+        textViewResumenArriendo = view.findViewById(R.id.ResumenArriendo);
+        textViewResumenAlimentacion = view.findViewById(R.id.ResumenAlimentacion);
+        textViewResumenTransporte = view.findViewById(R.id.ResumenTransporte);
+        textViewResumenSerBasicos = view.findViewById(R.id.ResumenSerBasicos);
+        textViewResumenEducacion = view.findViewById(R.id.ResumenEducacion);
+        textViewResumenDeudas = view.findViewById(R.id.ResumenDeudas);
+        textViewResumenAhorros = view.findViewById(R.id.ResumenAhorros);
+        textViewResumenOtros = view.findViewById(R.id.ResumenOtros);
+
+        // Obtener la instancia de SharedPreferences
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(requireContext());
+
+        // Obtener los valores de las preferencias y establecer los textos de los TextViews
+        String presupuestoTotal = sharedPreferences.getString("presupuestoTotal", "0");
+        String resumenArriendo = sharedPreferences.getString("PresuArriedo", "0");
+        String resumenAlimentacion = sharedPreferences.getString("PresuAlimentacion", "0");
+        String resumenTransporte = sharedPreferences.getString("PresuTransporte", "0");
+        String resumenSerBasico = sharedPreferences.getString("PresuServBasicos", "0");
+        String resumenEducacion = sharedPreferences.getString("PresuEducacion", "0");
+        String resumenDeudas = sharedPreferences.getString("PresuDeudas", "0");
+        String resumenAhorro = sharedPreferences.getString("PresuAhorro", "0");
+        String resumenOtros = sharedPreferences.getString("PresuOtros", "0");
+
+
+
+        textViewPresupuestoTotal.setText("Presupuesto Total: "+ presupuestoTotal);
+        textViewResumenArriendo.setText(resumenArriendo + " / ");
+        textViewResumenAlimentacion.setText(resumenAlimentacion + " / ");
+        textViewResumenTransporte.setText(resumenTransporte + " / ");
+        textViewResumenSerBasicos.setText(resumenSerBasico + " / ");
+        textViewResumenEducacion.setText(resumenEducacion + " / ");
+        textViewResumenDeudas.setText(resumenDeudas + " / ");
+        textViewResumenAhorros.setText(resumenAhorro + " / ");
+        textViewResumenOtros.setText(resumenOtros + " / ");
+
+        return view;
     }
 }
